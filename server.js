92135cba918277e7eb86deb0cb10c965cf3b5224
m-triple-m <triplem656@gmail.com>
2021-05-05 14:05:19 +0530

    ..

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const path = require('path');
 
+const fs = require('fs');
+
+console.log(fs.readdirSync(__dirname + '/dist/'));
+
 const app = express();
 
 // Serve only the static files form the dist directory
