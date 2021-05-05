705133b7cd4cc7bba7f7722438640270139b44f8
m-triple-m <triplem656@gmail.com>
2021-05-05 13:45:11 +0530

    ..

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/merch-fontend'));
+app.use(express.static(__dirname + '/dist/nebular-starter'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/merch-fontend/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/nebular-starter/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
